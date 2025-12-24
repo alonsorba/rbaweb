2f8e0d4a3daa8d5430aa94bc633e9d9aa126a80a
alonsorba <alonso@rbaidon.com.mx>
2025-12-24 12:03:51 -0700

    ajuste en logo y carrucel de imagenes

--- servicios.php
+++ servicios.php
@@ -16,7 +16,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="assets/css/style.css?v=2">
+  <link rel="stylesheet" href="assets/css/style.css?v=3">
 </head>
 
 <body>
@@ -280,4 +280,3 @@
   <script src="assets/js/main.js?v=4"></script>
 </body>
 </html>
-
